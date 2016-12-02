ff0099815ae3f814d0b14b0b8f973c0d6d690c9d
Jose Riguera <jriguera@gmail.com>
2016-12-02 19:06:03 +0100

    fix error when variable is not defined (-u bash flag)

--- jobs/ansible-bigiplb/templates/ansible/env.sh
+++ jobs/ansible-bigiplb/templates/ansible/env.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 # It needs to load the bigip_irule_bigsuds created by Ryan to manage irules without admin privileges
-export ANSIBLE_LIBRARY=/var/vcap/packages/ansible-bigiplb/library:${ANSIBLE_LIBRARY}
+if [ -z ${ANSIBLE_LIBRARY+x} ]; then
+    ANSIBLE_LIBRARY=/var/vcap/packages/ansible-bigiplb/library
+else
+    ANSIBLE_LIBRARY=/var/vcap/packages/ansible-bigiplb/library:${ANSIBLE_LIBRARY}
+fi
+export ANSIBLE_LIBRARY
 
